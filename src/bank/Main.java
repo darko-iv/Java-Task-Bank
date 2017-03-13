f72dd1843e391278c2d688df0121465762a5a276
darko.ivanovski <darko.ivanovski@DarkoI-L560.musala.com>
2017-03-13 10:23:07 +0100

    Java Task: Bank

--- src/bank/Main.java
+++ src/bank/Main.java
@@ -2,7 +2,7 @@ package bank;
 
 public class Main {
 	public static void main(String[] args) {
-		Transaction t = new Transaction(new Account("Darko", "ID:213123"), new Account("Mile", "ID:213123"), 5000.0,500.0);
+		Transaction t = new Transaction(new Account("Darko", "ID:213123"), new Account("Mile", "ID:213123"), 5000.0,550.0);
 		
 		Payment newPayment = new Payment();
 		newPayment.submit(t);
